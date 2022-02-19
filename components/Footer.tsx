e93d38d63d5d49c53e5b6a4de870df997e120eae
Sergen ORIN <serg_or5@hotmail.com>
2022-02-19 11:10:56 +0300

    fix Footer positioning, make links in Footer external

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     <Center
       style={{
+        position: 'absolute',
         width: '100%',
         height: '100px',
         bottom: '0px',
@@ -12,7 +13,18 @@ const Footer = () => {
       <footer style={{ padding: '10px' }}>
         <FrostedPaper style={{ textAlign: 'center' }}>
           <Text>
-            Built with <Anchor href="https://nextjs.org/">Next.js</Anchor> &amp; <Anchor href="https://mantine.dev/">Mantine</Anchor>. Powered by <Anchor href="https://vercel.com/">Vercel</Anchor>
+            Built with{' '}
+            <Anchor href="https://nextjs.org/" target="_blank">
+              Next.js
+            </Anchor>{' '}
+            &amp;{' '}
+            <Anchor href="https://mantine.dev/" target="_blank">
+              Mantine
+            </Anchor>
+            . Powered by{' '}
+            <Anchor href="https://vercel.com/" target="_blank">
+              Vercel
+            </Anchor>
           </Text>
         </FrostedPaper>
       </footer>
